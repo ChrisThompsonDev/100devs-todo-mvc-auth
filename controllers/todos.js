6dc63cf95cebb3110d6d0e81a49d0ce62a35519f
eggsmayhem <flipmybit@protonmail.com>
2022-08-31 14:07:34 -0400

    basic category logic for getTodo controller, and front-end rendering

--- controllers/todos.js
+++ controllers/todos.js
@@ -4,9 +4,21 @@ module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
+            //capture all current categories for the front end
+            const categories = []
+
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+
+            //populate categories array
+            todoItems.map(x => {
+                if (!categories.includes(x.category)) {
+                    categories.push(x.category)
+                }
+            })
+            console.log(categories)
+
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user, categories: categories})
         }catch(err){
             console.log(err)
         }
